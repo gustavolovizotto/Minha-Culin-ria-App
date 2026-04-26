d77832ba20c6977b845f2652185f83e5c8022442
gustavolovizotto <gustavolovizotto@gmail.com>
2026-04-26 19:21:42 -0300

    feat: adiciona telas de CRUD

--- app/src/main/java/com/example/minhaculinriaapp/ui/acervo/AcervoFragment.java
+++ app/src/main/java/com/example/minhaculinriaapp/ui/acervo/AcervoFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import com.example.minhaculinriaapp.R;
 
@@ -18,5 +19,16 @@ public class AcervoFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_acervo, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.fab_nova_receita).setOnClickListener(v ->
+                Navigation.findNavController(v).navigate(R.id.action_acervo_to_cadastro_receita));
+
+        view.findViewById(R.id.card_nova_categoria).setOnClickListener(v ->
+                Navigation.findNavController(v).navigate(R.id.action_acervo_to_cadastro_categoria));
     }
 }
